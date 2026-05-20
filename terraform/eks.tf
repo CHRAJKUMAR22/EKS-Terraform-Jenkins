fc243d6cf256cef05fb92791911394fdc34999e8
CHRAJKUMAR22 <97086614+CHRAJKUMAR22@users.noreply.github.com>
2026-05-20 14:07:56 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,16 +2,20 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
 
-  cluster_name                   = local.name
+  cluster_name    = local.name
+  cluster_version = "1.32"
+
   cluster_endpoint_public_access = true
 
   cluster_addons = {
     coredns = {
       most_recent = true
     }
+
     kube-proxy = {
       most_recent = true
     }
+
     vpc-cni = {
       most_recent = true
     }
@@ -21,7 +25,6 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
     instance_types = ["m5.large"]
@@ -30,13 +33,16 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
+
     amc-cluster-wg = {
+
       min_size     = 1
       max_size     = 2
       desired_size = 1
 
       instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
+
+      capacity_type = "SPOT"
 
       tags = {
         ExtraTag = "helloworld"
